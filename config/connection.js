01fd8cfe471513b2d65dc7d41b2b6354eced6c78
vinson-ridenour <vinsonridenour@Vinses-MacBook-Pro.local>
2017-07-27 16:12:52 -0700

    set up JAWSDB remote database

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
